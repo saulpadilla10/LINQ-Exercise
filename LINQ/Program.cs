fcd2733e4d2ddc5e33a9ac902347b9c2fb0bf8ff
saulpadilla10 <saulp1021@yahoo.com>
2023-06-15 17:22:41 -0700

    Video Game Lists

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using LINQ;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> vidGames= new List<string>
+            { "Mortal Kombat", "God of War", "Splinter Cell", "Super Smash Bros.", "Madden"};
+
+            int minLength = 0;
+
+            IEnumerable<string> longestNames = vidGames.Where(name => name.Length > minLength);
+
+            vidGames = vidGames.OrderBy(name => name.Length).ToList();
+
+            foreach (var name in vidGames)
+            {
+
+                Console.WriteLine(name);
+            }
+
         }
     }
 }
